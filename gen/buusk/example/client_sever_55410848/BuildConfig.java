0728dc60e2c98f7d4dd3cd9e0ee1f8c9de28b342
pangpimchanok <pang_pang_7421@hotmail.com>
2014-11-05 09:58:59 +0700

    cliant_sever

---
+++ gen/buusk/example/client_sever_55410848/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package buusk.example.client_sever_55410848;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
